6a8aea570c84db638f9d210322f7d5f7e4eccdcd
GrzegorzKy <59531376+GrzegorzKy@users.noreply.github.com>
2020-03-20 21:29:31 +0100

    CRUD
    Dodanie pełnej funkcjonalności CRUD - Update i Delete dla postów.

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,7 +2,7 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
@@ -128,8 +128,46 @@ def new_post():
         db.session.commit()
         flash('The post has been created.', 'success')
         return redirect(url_for('home'))
-    return render_template('create_post.html', title='Create a post', form=form)
-    
+    return render_template('create_post.html', title='Create a post', form=form,
+                           legend='Create a post')
+
+
+@app.route('/post/<int:post_id>', methods=['GET', 'POST'])
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if current_user != post.author:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Create a post', form=form,
+                           legend='Update post')
+
+
+@app.route('/post/<int:post_id>/udelete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if current_user != post.author:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted', 'success')
+    return redirect(url_for('home'))
     
     
     
